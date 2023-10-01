3a76141cf4aeaabe40ab09b5155f1ceef4aea081
mesn1985 <lightmaze16@hotmail.com>
2023-10-01 07:40:28 +0200

    initial commit

---
+++ ValidateTheNameWebApplication/Controllers/NameController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValidateTheNameWebApplication.Controllers
+{
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(){
+            return View("Index",false);
+        }
+
+        [HttpPost]
+        public IActionResult ValidateName(string name) { 
+            bool nameIsValid = NameIsValid(name);
+            return View("Index",nameIsValid);
+        }
+
+        private Boolean NameIsValid(string name){
+            //Insert logic here
+            if(name.Length < 4)
+                return false;
+            return true;
+        }
+    }
+}
