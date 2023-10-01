6074465d098234b57f4a73f1ad8669364002dc75
Martin Edwin Schjødt Nielsen <MESN@ucl.dk>
2023-10-01 14:55:47 +0200

    Exercise with invariance added

---
+++ InvarianceDTOandModel/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace ValidateTheNameModelBinding.Models
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            isFirstNameValid(firstName);
+            isLastNameValid(lastName);
+
+            this.firstName = firstName; 
+            this.lastName = lastName;
+        }
+
+        private void isFirstNameValid(string firstname)
+        {
+            ///Add Invariance validation Here
+            /// Throw an Exception if the name is invalid
+        }
+        private void isLastNameValid(string lastname)
+        {
+            ///Add Invariance validation Here
+            /// Throw an Exception if the name is invalid
+        }
+    }
+}
